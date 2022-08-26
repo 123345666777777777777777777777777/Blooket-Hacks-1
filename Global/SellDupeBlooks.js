9b740994f86e72fe8a345a36467b5bf23aec5773
codemaster123puppy <98063969+codemaster123puppy@users.noreply.github.com>
2022-08-26 16:42:07 -0700

    Update SellDupeBlooks.js

--- Global/SellDupeBlooks.js
+++ Global/SellDupeBlooks.js
@@ -59,7 +59,7 @@ function footer() {
     let element = document.createElement('div');
 
     element.style = `font-family: "Nunito", sans-serif; font-size: 14px; height: 65px; width: 175px; border: 4px solid rgb(15, 15, 15); background: rgb(240, 240, 240); position: absolute; top: 20x; left: 20px; border-radius: 10px; color: rgb(0, 0, 0); text-align: center;`;
-    element.innerHTML = `<p>Made by gliz <br> My <a style="color: #0000ff;" href="https://twitter.com/glizuwu" target="_blank">twitter</a></p>`;
+    element.innerHTML = `<p>Made by codemaster123puppy <br> My <a style="color: #0000ff;" href="https://ghithub.com/codemaster123puppy" target="_blank">github</a></p>`;
     document.body.appendChild(element);
 
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
